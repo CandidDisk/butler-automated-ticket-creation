b43888a7357c5b8cc1a647ee00dfe85017273118
CandidDisk <marktajima@me.com>
2023-06-09 12:11:11 +0800

    refactor(TicketController.ts): RequestHandler now async

--- src/controllers/TicketController.ts
+++ src/controllers/TicketController.ts
@@ -2,9 +2,9 @@ import {NextFunction, Request, RequestHandler, Response, response} from 'express
 import {Ticket} from "../models/TicketInterface";
 import {createTicket} from "../services/TicketService";
 
-const postTicket: RequestHandler = (req: Request<{}, {}, Ticket>, res: Response, next: NextFunction) => {
+const postTicket: RequestHandler = async (req: Request<{}, {}, Ticket>, res: Response, next: NextFunction) => {
     try {
-        const response = createTicket(req.body)
+        const response = await createTicket(req.body)
         res.status(response.status.code).send(response.body)
     } catch (e) {
         res.status(500).send(e)
